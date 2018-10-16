b03151fd6726a360800b4b76616c2804987b50a1
Steve Trap <trap.steve@hotmail.com>
2018-10-16 17:27:23 +0200

    rescaffolded Device with dev_eui and not dev-eui

---
+++ app/views/devices/_device.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! device, :id, :dev_eui, :Location_id, :created_at, :updated_at
+json.url device_url(device, format: :json)
---
+++ db/migrate/20181016152624_create_devices.rb
@@ -0,0 +1,10 @@
+class CreateDevices < ActiveRecord::Migration[5.2]
+  def change
+    create_table :devices do |t|
+      t.string :dev_eui
+      t.references :Location, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
