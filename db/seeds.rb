b739223addd9a6cb1cb91967689627899e82c262
Steve Trap <trap.steve@hotmail.com>
2018-10-17 15:11:29 +0200

    added simple seeder for loc, devices, locations

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+location_list = [
+  [ "Afrika", "Hier is het warm" ],
+  [ "Amerika", "Hier woont Trump" ],
+  [ "Himalaya", "Hoog en droog EN KOUD!" ],
+  [ "Woestijn", "Hier is het heel warm" ]
+]
+
+location_list.each do |name, description|
+  Location.create( name: name, description: description )
+end
+
+for i in 0..4
+    Device.create(dev_eui: "" + i.to_s, Location: Location.find(i + 1))
+end
+
+temperature_list = [
+  [ "30.2", "C", 1 ],
+  [ "68", "F", 2 ],
+  [ "-10", "C", 3 ],
+  [ "40", "C", 4 ]
+]
+
+temperature_list.each do |value, unit, device|
+    Temperature.create( value: value, unit: unit, Device: Device.find(device) )
+end
