76603c8f09aa206ffc56b8a10efdcf8eabee80e1
Steve Trap <trap.steve@hotmail.com>
2018-10-23 13:10:22 +0200

    added comma

--- app/javascript/vue.js
+++ app/javascript/vue.js
@@ -1,7 +1,7 @@
 import Vue from 'vue/dist/vue.esm'
 document.addEventListener('DOMContentLoaded', () => {
     const app = new Vue({
-        el:'#app'
+        el:'#app',
         data: {
             message: 'Vue, what else!'
         }
