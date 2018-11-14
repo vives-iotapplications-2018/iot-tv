b01b4c8607191e50c6cad15950921ebf046503ab
Michiel Meyfroodt <michiel.pitou@gmail.com>
2018-11-14 16:26:09 +0100

    Added route for image response

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   resources :devices
   resources :temperatures
   resources :locations
+  get '/api/image', to: 'test_image#test_post'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
