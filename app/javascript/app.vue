da79b8b1058dccb6e0597f585dbaf49347a91ab0
Steve Trap <trap.steve@hotmail.com>
2018-11-23 11:44:55 +0100

    pushing timedata into labels

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -19,8 +19,9 @@ export default {
 
   data() {
     return {
-      labels: ["1h", "2h", "3h", "4h", "5h", "6h", "7h", "8h", "9h", "10h", "11h", "12h", 
-      "13h", "14h", "15h", "16h", "17h", "18h", "19h", "20h", "21h", "22h", "23h", "24h"],
+      labels: [],
+    /*   labels: ["1h", "2h", "3h", "4h", "5h", "6h", "7h", "8h", "9h", "10h", "11h", "12h", 
+      "13h", "14h", "15h", "16h", "17h", "18h", "19h", "20h", "21h", "22h", "23h", "24h"], */
       datasets: [
         {
           data: [],
@@ -39,6 +40,7 @@ export default {
           var arraySize = response.data.length; 
           for(var i = 0; i < arraySize; i++){
             this.datasets[0].data.push(response.data[i].value);
+            this.labels.push(response.data[i].created_at)
         }   
       })
     }, 
