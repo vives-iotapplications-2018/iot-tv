230059eb59cc9f19528dded5a8d7fc725a1b22e0
lenny vanderlinden <lenny_vanderlinden@hotmail.com>
2018-11-30 11:20:39 +0100

    last temperature shown on webpage

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -6,41 +6,46 @@
     v-bind:labels="labels"
     v-bind:bind="true"
   ></chartjs-line>
-<!--     <div class="temp-area" v-if="temperatures">
-        <h2>{{ temperatures }}</h2>
-
-      </div> -->
+    <div class="temp-area">
+       <!-- {{ datasets[0].data[27]}} -->
+       {{"Current Temperature " + lastvalue + " °C"}}
+      </div>
 </div>
 </template>
 
 <script>
 export default {
 
-
   data() {
     return {
+      lastvalue: 0,
       labels: [],
     /*   labels: ["1h", "2h", "3h", "4h", "5h", "6h", "7h", "8h", "9h", "10h", "11h", "12h", 
       "13h", "14h", "15h", "16h", "17h", "18h", "19h", "20h", "21h", "22h", "23h", "24h"], */
       datasets: [
-        {
+        { 
           data: [],
         },
       ],
+
       option: {
         title: {
           display: true,
           text: "Temperature chart"
         },
-      }
+      },
     };
   },
       created ()  {
         this.axios.get('http://localhost:3000/temperatures.json').then((response, error) => {
-          var arraySize = response.data.length; 
+          var arraySize = response.data.length;
+
           for(var i = 0; i < arraySize; i++){
             this.datasets[0].data.push(response.data[i].value);
             this.labels.push(response.data[i].created_at)
+            if(i == arraySize-1){
+              this.lastvalue = response.data[i].value
+            }
         }   
       })
     }, 
