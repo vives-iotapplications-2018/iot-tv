0412d4993648cbcb2b1a44927432bde7d5750fa9
Steve Trap <trap.steve@hotmail.com>
2018-12-09 14:41:54 +0100

    setting up websocket

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
   get 'welcome/index'
- 
   root 'welcome#index'
+  post 'temperatures' => 'temperatures#temperature', as: 'temperature'
   resources :devices
   resources :temperatures
   resources :locations
   post '/api/image', to: 'test_image#test_post'
+
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
